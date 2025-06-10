979cd934595feeb9db56414e7e1f67bbc68aea0b
ignarru <arruvitoignacio@gmail.com>
2025-06-10 14:40:18 -0300

    Update Hero.tsx

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -84,7 +84,7 @@ export default function Hero() {
         setTimeout(() => {
           setBrainHidden(true);
           setButtonHidden(true);
-       }, 8000);
+       }, 800);
       }, 600);
     }, 1000);
   };
@@ -135,7 +135,7 @@ export default function Hero() {
           </AnimatePresence>
         </motion.div>
         
-        <div className="relative mt-4 md:mt-2 lg:mt-0 xl:-mt-2 h-52 sm:h-60 md:h-72 lg:h-80 xl:h-96 2xl:h-[28rem] flex flex-col items-center justify-center transform -translate-y-6 sm:-translate-y-8 md:-translate-y-10 lg:-translate-y-12 xl:-translate-y-16 2xl:-translate-y-20">
+        <div className="relative mt-4 md:mt-2 lg:mt-0 xl:-mt-2 h-52 sm:h-60 md:h-72 lg:h-80 xl:h-96 2xl:h-[28rem] flex flex-col items-center justify-center transform -translate-y-12 sm:-translate-y-16 md:-translate-y-20 lg:-translate-y-24 xl:-translate-y-28 2xl:-translate-y-32">
           {!brainHidden && (
             <div
               className={`absolute inset-0 flex justify-center items-center transition-all duration-1000 delay-300 ${
