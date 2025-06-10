a7b6979525ead9d77c664f1a42565a7aebf6b7d6
ignarru <arruvitoignacio@gmail.com>
2025-06-10 18:31:45 -0300

    Update Hero.tsx

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -131,7 +131,11 @@ export default function Hero() {
           </AnimatePresence>
         </motion.div>
         
-        <div className="relative mt-8 md:mt-2 lg:mt-0 xl:-mt-2 h-52 sm:h-60 md:h-72 lg:h-80 xl:h-96 2xl:h-[28rem] flex flex-col items-center justify-center transform -translate-y-16 sm:-translate-y-20 md:-translate-y-24 lg:-translate-y-28 xl:-translate-y-32 2xl:-translate-y-36">
+        <div
+          className={`relative mt-8 md:mt-2 lg:mt-0 xl:-mt-2 h-52 sm:h-60 md:h-72 lg:h-80 xl:h-96 2xl:h-[28rem] flex flex-col items-center justify-center transform -translate-y-16 sm:-translate-y-20 md:-translate-y-24 lg:-translate-y-28 xl:-translate-y-32 2xl:-translate-y-36 ${
+            started ? "pointer-events-auto" : "pointer-events-none"
+          }`}
+        >
           {!brainHidden && (
             <div
               className={`absolute inset-0 flex justify-center items-center transition-all duration-1000 delay-300 ${
