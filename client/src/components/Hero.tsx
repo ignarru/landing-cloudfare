ef883c979e4fc2ae97b02e8b4a59ea86cc80a56d
ignarru <arruvitoignacio@gmail.com>
2025-06-11 01:54:09 -0300

    Update Hero.tsx

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -69,7 +69,7 @@ const [brainAscending, setBrainAscending] = useState(false);
   const handleStartClick = () => {
     if (started) return;
     setStarted(true);
-    setTimeout(() => setButtonHidden(true), 300);
+    setTimeout(() => setButtonHidden(true), 600);
     setStarsExpanded(true);
     setTimeout(() => setStarsExpanded(false), 800);
     setBrainActive(true);
