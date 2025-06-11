1b6cf75743e68ccbbf4c134aba97ea059dd017fb
ignarru <arruvitoignacio@gmail.com>
2025-06-11 00:21:03 -0300

    Update WhatsappButton.tsx

--- client/src/components/WhatsappButton.tsx
+++ client/src/components/WhatsappButton.tsx
@@ -4,7 +4,7 @@ export default function WhatsappButton() {
   return (
     <a
       href="https://wa.me/5491160390755"
-      className="fixed bottom-4 right-4 z-50 p-4 bg-green-500 hover:bg-green-600 text-white rounded-full shadow-lg"
+      className="fixed bottom-4 right-4 z-50 p-4 bg-green-500 hover:bg-green-600 text-white rounded-full shadow-lg transform hover:scale-105 transition-transform animate-bounce-slow hover:animate-none"
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Chatea en WhatsApp"
