82aaa0f3bf3956c61abbb9dd2792d660c16f6962
ignarru <arruvitoignacio@gmail.com>
2025-06-11 00:09:44 -0300

    Create WhatsappButton.tsx

---
+++ client/src/components/WhatsappButton.tsx
@@ -0,0 +1,15 @@
+import { FaWhatsapp } from "react-icons/fa";
+
+export default function WhatsappButton() {
+  return (
+    <a
+      href="https://wa.me/5491160390755"
+      className="fixed bottom-4 right-4 z-50 p-4 bg-green-500 hover:bg-green-600 text-white rounded-full shadow-lg"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="Chatea en WhatsApp"
+    >
+      <FaWhatsapp className="w-6 h-6" />
+    </a>
+  );
+}
